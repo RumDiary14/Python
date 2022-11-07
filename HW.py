63c9654038020fb0341beed7738189bccb9ffcb0
Roman Davidyuk <davidyuk.roman98@gmail.com>
2022-11-07 22:16:48 +0200

    modified in new branch

--- HW.py
+++ HW.py
@@ -19,6 +19,22 @@ else:
     print("unknown animal ")
 
 
+num = -781
+type = ""
+type2 = ""
+
+if abs(num) // 10 == 0:
+    type = "1"
+elif abs(num) // 10 < 10:
+    type = "2"
+else:
+    type = "3"
+if num > 0:
+    type2 = "positive"
+else:
+    type2 = "negative"
+print("it is zero") if not num else print("it is a " + type2 + " value with " + type + " num")
+
 
 
 
