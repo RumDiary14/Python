1994b4a10ff6b94944c86f1cc11fa349d424cdc4
Roman Davidyuk <davidyuk.roman98@gmail.com>
2022-11-07 20:48:06 +0200

    HW is done

--- HW.py
+++ HW.py
@@ -6,3 +6,12 @@ else:
 
 a = 5
 print("even number") if a / 2 == 0 else print("odd number")
+
+
+sound = "bark"
+if sound == "meow":
+    print("cat")
+elif sound == "bark":
+    print("dog")
+else:
+    print("unknown animal ")
